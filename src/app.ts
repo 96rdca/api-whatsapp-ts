29a23ebab4ec5bc1f7b19942da089b17792d1a58
Richard Casado <rcasado@lionbridge.com.do>
2023-10-18 00:53:09 -0400

    add heroku.yml

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('tmp'))
 app.use(`/`, routes)
+// app.use((req, res, next) => {
+//     const currentTime = new Date();
+//     const startHour = 8;
+//     const endHour = 17;
+
+//     if(currentTime.getHours() >= startHour && currentTime.getHours() < endHour ){
+//         next();
+//     }else{
+//         res.status(403).send('API is only available from 8 am to 5 pm');
+//     }
+// })
 
 app.listen(port, () => console.log(`Ready... @port:${port}`))
